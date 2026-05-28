247cec6b7c74dc128f786a7487def4cca3ca335d
Cate James <catejames@cates-air-4920.wlan.dur.ac.uk>
2026-05-28 12:56:18 +0100

    Multiply Check and Dynamic Dimensions

--- main.go
+++ main.go
@@ -18,19 +18,55 @@ func main() {
 	fmt.Println(result)
 }
 
-func Multiply(A [][]float64, B [][]float64) [][]float64 {
-
-	result := make([][]float64, 2)
-	for i := range result {
-		result[i] = make([]float64, 2)
+func CanMultiply(A [][]float64, B [][]float64) bool {
+	if len(A) == 0 || len(B) == 0 {
+		return false
 	}
 
-	for i := 0; i < 2; i++ {
-		for j := 0; j < 2; j++ {
+	// ensure matrix A is a proper rectangle
+	colsA := len(A[0])
+	for _, row := range A {
+		if len(row) != colsA {
+			return false // found jagged matrix
+		}
+	}
+
+	// ensure matirx B is a proper rectangle
+	colsB := len(B[0])
+	for _, row := range B {
+		if len(row) != colsB {
+			return false // found jagged matrix
+		}
+	}
+
+	// check standard matrix multiplication
+	return colsA == len(B)
+}
+
+func Multiply(A [][]float64, B [][]float64) [][]float64 {
+
+	// run check
+	if !CanMultiply(A, B) {
+		fmt.Println("Cannot multiply the given matrices.")
+		return [][]float64{} // return empty matrix
+	}
+
+	// multiplication using dynamic dimensions
+	rowsA := len(A)
+	colsA := len(A[0])
+	colsB := len(B[0])
+
+	result := make([][]float64, rowsA)
+	for i := range result {
+		result[i] = make([]float64, colsB)
+	}
+
+	for i := 0; i < rowsA; i++ {
+		for j := 0; j < colsB; j++ {
 
 			sum := 0.0
 
-			for k := 0; k < 2; k++ {
+			for k := 0; k < colsA; k++ {
 				sum += A[i][k] * B[k][j]
 			}
 
